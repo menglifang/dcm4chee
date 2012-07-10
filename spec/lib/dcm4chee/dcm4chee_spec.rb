56958e7a7b595e6fbee404eab05b46d813570ca8
Tower He <towerhe@gmail.com>
2012-07-11 03:42:40 +0800

    `repository` can not be the key

--- spec/lib/dcm4chee/dcm4chee_spec.rb
+++ spec/lib/dcm4chee/dcm4chee_spec.rb
@@ -2,19 +2,21 @@ require 'spec_helper'
 
 describe Dcm4chee do
   describe '.configure' do
-    before(:each) do
+    before(:all) do
+      @config = Dcm4chee.config.clone
+
       Dcm4chee.configure do
         jolokia_url 'http://localhost:8080/jolokia'
 
-        repository do
+        repo do
           adapter 'sqlite3'
           database './db/dcm4chee.sqlite3'
         end
       end
     end
 
-    after(:each) do
-      Dcm4chee.instance_variable_set(:@config, nil)
+    after(:all) do
+      Dcm4chee.instance_variable_set(:'@config', @config)
     end
 
     subject { Dcm4chee.config }
@@ -22,11 +24,11 @@ describe Dcm4chee do
     its(:jolokia_url) { should == 'http://localhost:8080/jolokia' }
 
     it 'sets the adapter of the repository' do
-      expect(subject.repository.adapter).to eql('sqlite3')
+      expect(subject.repo.adapter).to eql('sqlite3')
     end
 
     it 'sets the database of the repository' do
-      expect(subject.repository.database).to eql('./db/dcm4chee.sqlite3')
+      expect(subject.repo.database).to eql('./db/dcm4chee.sqlite3')
     end
   end
 end
