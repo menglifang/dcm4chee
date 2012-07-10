ad95aea1a7254a4e66ebb630e8097c2370b2d794
Tower He <towerhe@gmail.com>
2012-07-11 01:54:42 +0800

    Proxy of dcm4chee.archive:service=ContentEditService MBean

---
+++ lib/dcm4chee/content_edit_service.rb
@@ -0,0 +1,40 @@
+module Dcm4chee
+  class ContentEditService
+
+    MBEAN_NAME = 'dcm4chee.archive:service=ContentEditService'
+
+    attr_accessor :jolokia
+
+    def initialize(jolokia)
+      @jolokia = jolokia
+    end
+
+    def empty_trash
+      jolokia.execute(MBEAN_NAME, 'emptyTrash')
+    end
+
+    class << self
+      def define_component(name)
+        define_method("move_#{name}_to_trash".to_sym) do |id|
+          jolokia.execute(MBEAN_NAME,
+                          "move#{name}ToTrash(long)", id)
+        end
+
+        define_method("undelete_#{name}".to_sym) do |id|
+          jolokia.execute(MBEAN_NAME,
+                          "undelete#{name}(long)", id)
+        end
+
+        define_method("delete_#{name}".to_sym) do |id|
+          jolokia.execute(MBEAN_NAME,
+                          "delete#{name}(long)", id)
+        end
+      end
+    end
+
+    define_component :patient
+    define_component :study
+    define_component :series
+    define_component :instance
+  end
+end
