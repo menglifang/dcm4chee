00adc39670bf9712bc8ed006068d5e428f68044d
Tower He <towerhe@gmail.com>
2012-07-11 03:42:53 +0800

    Config test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,17 @@ require File.join(File.dirname(__FILE__), 'dummy/config/environment')
 require 'rspec/rails'
 require 'rspec/autorun'
 
-require 'dm-migrations'
+require 'factory_girl_rails'
+require 'pry'
+
+require 'dcm4chee'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join('support/**/*.rb')].each {|f| require f}
+
+DataMapper.setup(:default, 'sqlite::memory:')
+DataMapper.setup(:dcm4chee, 'sqlite::memory:')
 
 RSpec.configure do |config|
   # ## Mock Framework
@@ -29,6 +35,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  config.include FactoryGirl::Syntax::Methods
 
   config.before(:each) { DataMapper.auto_migrate! }
 end
