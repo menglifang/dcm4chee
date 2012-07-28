c4a5191136fdba56a95ad86457733bdcf25e9d5d
Tower He <towerhe@gmail.com>
2012-07-28 23:09:58 +0800

    Remove trashed components

--- lib/dcm4chee/services/content_edit_service.rb
+++ lib/dcm4chee/services/content_edit_service.rb
@@ -12,21 +12,18 @@ module Dcm4chee
         def define_component(name)
           name = name.to_s
 
-          if name.start_with?('trashed')
-            define_method("undelete_#{name}".to_sym) do |id|
-              jolokia.execute(MBEAN_NAME,
-                              "undelete#{name.camelize}(long)", id)
-            end
+          define_method("undelete_#{name}".to_sym) do |id|
+            jolokia.execute(MBEAN_NAME,
+                            "undelete#{name.camelize}(long)", id)
+          end
 
-            define_method("delete_#{name}".to_sym) do |id|
-              jolokia.execute(MBEAN_NAME,
-                              "delete#{name.camelize}(long)", id)
-            end
-          else
-            define_method("move_#{name}_to_trash".to_sym) do |id|
-              jolokia.execute(MBEAN_NAME,
-                              "move#{name.camelize}ToTrash(long)", id)
-            end
+          define_method("delete_#{name}".to_sym) do |id|
+            jolokia.execute(MBEAN_NAME,
+                            "delete#{name.camelize}(long)", id)
+          end
+          define_method("move_#{name}_to_trash".to_sym) do |id|
+            jolokia.execute(MBEAN_NAME,
+                            "move#{name.camelize}ToTrash(long)", id)
           end
         end
       end
@@ -35,10 +32,6 @@ module Dcm4chee
       define_component :study
       define_component :series
       define_component :instance
-
-      define_component :trashed_patient
-      define_component :trashed_study
-      define_component :trashed_series
     end
   end
 end
