5ee91b9f84047d348ec86ba7148a5e06c64977ba
Tower He <towerhe@gmail.com>
2013-02-01 15:11:00 +0800

    Pending the spec if the service is not available.

--- spec/api/vi/create_application_entities_spec.rb
+++ spec/api/vi/create_application_entities_spec.rb
@@ -3,12 +3,16 @@ require 'spec_helper'
 describe 'Creating application entities', type: :request do
   context 'with valid attributes' do
     it 'creates an application entity' do
-      post '/dcm4chee/api/application_entities', format: :json,
-        application_entity: {
-          title: 'CDRecorder',
-          host: '192.168.1.250',
-          port: '1234'
-        }
+      begin
+        post '/dcm4chee/api/application_entities', format: :json,
+          application_entity: {
+            title: 'CDRecorder',
+            host: '192.168.1.250',
+            port: '1234'
+          }
+      rescue
+        pending 'You should deploy a dcm4chee server with jolokia at http://localhost:8080/jolokia. see spec_helper.rb'
+      end
 
       response.status.should == 201
     end
